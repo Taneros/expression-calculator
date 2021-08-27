d0824382b45671e1ccce70aa0984ad14b4c5a16f
Taneros <fricentaurus@GMAIL.COM>
2021-08-28 00:58:51 +0300

    feat: add subtraction and regex

--- src/index-my.js
+++ src/index-my.js
@@ -1,13 +1,17 @@
 function expressionCalculator(expr) {
   // write your solution here
-  console.log('expression', expr)
+
+  expr = expr.replace(/\s/g, '')
+  //console.log('no spaces', expr)
+
+  //console.log('expression', expr)
 
   const operStack = []
 
   const exitArr = []
 
-  const exprArr = expr.split(' ')
-  console.log('exprArr', exprArr)
+  const exprArr = expr.split(/(\D)/)
+  //console.log('exprArr', exprArr)
 
   const operPriority = {
     1: ['*', '/'],
@@ -19,55 +23,55 @@ function expressionCalculator(expr) {
 
     // check if elem is number -> push to exit arr
     if (!isNaN(i)) {
-      console.log(`${i} is Number! -> push to exit arr`)
+      //console.log(`${i} is Number! -> push to exit arr`)
       exitArr.push(i)
-      console.log('exitArr', exitArr)
+      //console.log('exitArr', exitArr)
     }
     // this is operand -> need to push to oper stack or exit arr
     else {
       // if operaation stack is empty -> push operation arr
-      console.log(`${i} isNaN!`)
+      //console.log(`${i} isNaN!`)
       if (operStack.length === 0) {
-        console.log(`operStack empty! <- push ${i}`)
+        //console.log(`operStack empty! <- push ${i}`)
         operStack.push(i)
-        console.log('operStack', operStack)
+        //console.log('operStack', operStack)
       }
       // if not empty -> check priority using operPriority dictionary
       else {
 
-        console.log('operStack not empty!')
+        //console.log('operStack not empty!')
         let operStackLast = operStack[operStack.length - 1]
-        console.log('last value in operStack', operStack[operStack.length - 1])
+        //console.log('last value in operStack', operStack[operStack.length - 1])
         // check priority of current i element
 
         let elemPriorityOne = operPriority[1].includes(i) // true || false
-        console.log(` elemPriorityOne ${elemPriorityOne}`)
+        //console.log(` elemPriorityOne ${elemPriorityOne}`)
 
         // check priority of last element in stack
         let lastStackElPriorityOne = operPriority[1].includes(operStackLast) // true || false
-        console.log(` lastStackElPriorityOne ${lastStackElPriorityOne}`)
+        //console.log(` lastStackElPriorityOne ${lastStackElPriorityOne}`)
 
         // equal case
         // if both true or both false -> current element is of equal priority to last elem in stack -> pop() last elem from stack and push() to exit array and push() there current elem
         if (elemPriorityOne && lastStackElPriorityOne || !elemPriorityOne && !lastStackElPriorityOne) {
           exitArr.push(operStack.pop(operStackLast))
-          console.log('operStack.pop()', operStack, 'exitArr', exitArr)
+          //console.log('operStack.pop()', operStack, 'exitArr', exitArr)
           operStack.push(i)
-          console.log('operStack if1', operStack)
+          //console.log('operStack if1', operStack)
 
         }
         // last value in stack has higher priority && elem to add lower
         // if last elem in stack has higher priority -> then pop() and push() it to exit array -> push() there current elem
         else if (lastStackElPriorityOne && !elemPriorityOne) {
           exitArr.push(operStack.pop(operStackLast))
-          console.log('operStack.pop()', operStack, 'exitArr', exitArr)
+          //console.log('operStack.pop()', operStack, 'exitArr', exitArr)
           operStack.push(i)
-          console.log('operStack if2', operStack)
+          //console.log('operStack if2', operStack)
 
         }
         else {
           operStack.push(i)
-          console.log('operStack else', operStack)
+          //console.log('operStack else', operStack)
         }
       }
     }
@@ -78,7 +82,7 @@ function expressionCalculator(expr) {
     exitArr.push(elem)
   }
 
-  console.log('exit array', exitArr)
+  //console.log('exit array', exitArr)
   // do math operations
 
   let calcArr = []
@@ -88,50 +92,67 @@ function expressionCalculator(expr) {
 
     let lastTwo
 
-    calcArr.length >= 2 ? lastTwo = calcArr.slice(-2) :
-
-      console.log('elem', elem)
+    if (calcArr.length >= 2) {
+      lastTwo = calcArr.slice(-2)
+      //console.log('lastTwo', lastTwo)
+    }
+    //console.log('elem', elem)
 
     let elemInt = parseInt(elem)
 
-    console.log('elem of exitArr', elemInt)
-    console.log('calcArr', calcArr)
+    //console.log('elem of exitArr', elemInt)
+    //console.log('calcArr', calcArr)
 
     if (!isNaN(elemInt)) {
-      console.log('number!')
+      //console.log('number!')
       calcArr.push(elemInt)
-      console.log('calcArr', calcArr)
+      //console.log('calcArr', calcArr)
     }
     if (isNaN(elemInt)) {
-      console.log('math operation!')
+      //console.log('math operation!')
       switch (elem) {
         case '*':
           result = lastTwo.reduce((acc, el) => acc * el, 1)
-          console.log('result *', result)
+          //console.log('result *', result)
           for (let i = 0; i < 2; i++) {
             calcArr.pop()
-            console.log('pop()', calcArr)
+            //console.log('pop()', calcArr)
           }
           calcArr.push(result)
-          console.log('calcArr.push(result)', calcArr)
+          //console.log('calcArr.push(result)', calcArr)
           break
         case '+':
           result = lastTwo.reduce((acc, el) => acc + el, 0)
           for (let i = 0; i < 2; i++) {
             calcArr.pop()
-            console.log('pop()', calcArr)
+            //console.log('pop()', calcArr)
           }
           calcArr.push(result)
-          console.log('calcArr.push(result)', calcArr)
+          //console.log('calcArr.push(result)', calcArr)
           break
         case '/':
-          result = Math.floor(lastTwo[0] / lastTwo[1])
+          if (lastTwo[1] === 0) {
+            throw "TypeError: Division by zero."
+          }
+          result = lastTwo[0] / lastTwo[1]
           for (let i = 0; i < 2; i++) {
             calcArr.pop()
-            console.log('pop()', calcArr)
+            //console.log('pop()', calcArr)
           }
           calcArr.push(result)
-          console.log('calcArr.push(result)', calcArr)
+          //console.log('calcArr.push(result)', calcArr)
+          break
+        case '-':
+          if (lastTwo[1] === 0) {
+            throw "TypeError: Division by zero."
+          }
+          result = lastTwo[0] - lastTwo[1]
+          for (let i = 0; i < 2; i++) {
+            calcArr.pop()
+            //console.log('pop()', calcArr)
+          }
+          calcArr.push(result)
+          //console.log('calcArr.push(result)', calcArr)
           break
       }
 
@@ -139,14 +160,16 @@ function expressionCalculator(expr) {
   }
 
   return calcArr
-
 }
 
-console.log(expressionCalculator('3 / 3 + 3 / 3 + 2 * 10 + 54 / 65'))
-// console.log(expressionCalculator('2 * 5'))
+// //console.log(expressionCalculator('4 / 2'))
+// //console.log(expressionCalculator('88 - 72 + 55 * 57'))
+console.log(expressionCalculator('4 - 2 + 5 * 6'))
 
-// console.log(expressionCalculator('49 * 63 / 58 * 36'))
-// console.log(expressionCalculator('5 * 2 + 10'))
+// //console.log(expressionCalculator('2 * 5'))
+
+// //console.log(expressionCalculator('49 * 63 / 58 * 36'))
+// //console.log(expressionCalculator('5 * 2 + 10'))
 
 
 
